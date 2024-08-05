60a723d86321f0de3de62cb6fe1a73103947511c
Xuan Wang <xuanwn@google.com>
2024-08-05 17:58:21 +0000

    Only test app_net_csm_observability_test

--- .kokoro/psm_interop_kokoro_lib.sh
+++ .kokoro/psm_interop_kokoro_lib.sh
@@ -259,12 +259,6 @@ psm::csm::setup() {
 #######################################
 psm::csm::get_tests() {
   TESTS=(
-    "gamma.gamma_baseline_test"
-    "gamma.gamma_grpcroute_test"
-    "gamma.affinity_test"
-    "gamma.affinity_session_drain_test"
-    "gamma.csm_observability_test"
-    "app_net_ssa_test"
     "app_net_csm_observability_test"
   )
 }
